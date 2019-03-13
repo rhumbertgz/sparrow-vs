520b4f7c0b7e9981f174abc14f8d1a6d7b611e74
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2019-03-14 00:03:04 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf sparrow-vs-0.1.0.vsix
+rm -rf sparrow-ls-release
+
+asdf global erlang 19.3.6.9
+asdf global elixir 1.7.0
+vsce package
+asdf global erlang 21.3
+asdf global elixir 1.8.1
