fd742927c63fd9bf0201c7767f89da65b602e4d0
Humberto Rodriguez Avila <rhumbert@vub.ac.be>
2019-03-14 00:19:13 +0100

    Add dependencies of submodules

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ rm -rf sparrow-ls-release
 
 asdf global erlang 19.3.6.9
 asdf global elixir 1.7.0
+cd sparrow-ls && mix deps.get && mix compile
+cd ..
 vsce package
 asdf global erlang 21.3
 asdf global elixir 1.8.1
